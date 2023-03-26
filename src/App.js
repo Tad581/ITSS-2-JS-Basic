22b2572b29b710b924c1c3fe4b5903de7e009a0f
tadd582 <nguyendat58211@gmail.com>
2023-03-26 18:05:58 +0700

    chore: add fontawesome icon

--- src/App.js
+++ src/App.js
@@ -1,62 +1,10 @@
-import { useReducer, useRef } from "react";
+import { useReducer, useRef, useEffect } from "react";
+import { states } from "./reducer/states";
+import reducer from "./reducer";
 
 const initState = {
   job: "",
   jobs: [],
-};
-
-const SET_JOB = "set_job";
-const ADD_JOB = "add_job";
-const DELETE_JOB = "delete_job";
-
-const setJob = (payload) => {
-  return {
-    type: SET_JOB,
-    payload,
-  };
-};
-
-const addJob = (payload) => {
-  return {
-    type: ADD_JOB,
-    payload,
-  };
-};
-
-const deleteJob = (payload) => {
-  return {
-    type: DELETE_JOB,
-    payload,
-  };
-};
-
-const reducer = (state, action) => {
-  let newState;
-  switch (action.type) {
-    case SET_JOB:
-      newState = {
-        ...state,
-        job: action.payload,
-      };
-      break;
-    case ADD_JOB:
-      newState = {
-        ...state,
-        jobs: [...state.jobs, action.payload],
-      };
-      break;
-    case DELETE_JOB:
-      const newJobs = [...state.jobs];
-      newJobs.splice(action.payload, 1);
-      newState = {
-        ...state,
-        jobs: [...newJobs],
-      };
-      break;
-    default:
-      throw new Error("Invalid action");
-  }
-  return newState;
 };
 
 function App() {
@@ -64,10 +12,19 @@ function App() {
 
   const { job, jobs } = state;
 
+  useEffect(() => {
+    const jobs = JSON.parse(window.localStorage.getItem("jobs"));
+    console.log("🚀 ~ file: App.js:17 ~ useEffect ~ jobs:", jobs)
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem("jobs", JSON.stringify(state));
+  }, [state]);
+
   const inputRef = useRef();
   const handleSubmit = () => {
-    dispatch(addJob(job));
-    dispatch(setJob(""));
+    dispatch(states.addJob(job));
+    dispatch(states.setJob(""));
 
     inputRef.current.focus();
   };
@@ -81,7 +38,7 @@ function App() {
           value={job}
           placeholder="Enter todo..."
           onChange={(e) => {
-            dispatch(setJob(e.target.value));
+            dispatch(states.setJob(e.target.value));
           }}
         />
         <button onClick={handleSubmit}> Add </button>
@@ -89,7 +46,9 @@ function App() {
           {jobs.map((job, index) => (
             <li key={index}>
               {job}
-              <span onClick={() => dispatch(deleteJob(index))}>&times;</span>
+              <span onClick={() => dispatch(states.deleteJob(index))}>
+                &times;
+              </span>
             </li>
           ))}
         </ul>
